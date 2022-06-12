89808adad91802b3ff62db2d1b4a70295ba7afd1
wrp801 <wes.poulsen@gmail.com>
2022-06-12 16:52:27 -0600

    Working rust implementation

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -17,21 +17,17 @@ fn filter_map(input: HashMap<String,i32>,amount:i32) -> HashMap<String,i32> {
 }
 
 fn print_map(map: HashMap<String,i32>) {
-    /**
-     * Prints a hashmap to the console in a nice format 
-     */
     for (k,v) in map.iter() {
-        println!("{}: {}",k,v)
+        if *v > 0 {
+            println!("{}: {}",k,v)
+        }
     }
 }
 
-fn select_max(input: HashMap<String,i32>,amount:i32) -> HashMap<String,i32> {
-    /**
-     * Selects the maximum key-value pair from the input map argument
-     */
+fn select_max(input: HashMap<String,i32>,amount:i32) -> (String,i32) {
 
     let filtered_map = filter_map(input,amount);
-    let mut ret_map = HashMap::new();
+    // let mut ret_map = HashMap::new();
     let mut key:String = String::from("");
     let mut val:i32 = 0;
     for (k,v) in filtered_map {
@@ -41,8 +37,8 @@ fn select_max(input: HashMap<String,i32>,amount:i32) -> HashMap<String,i32> {
         }
 
     }
-    ret_map.insert(key,val);
-    return ret_map;
+    // ret_map.insert(key,val);
+    return (key,val);
 }
 
 
@@ -59,17 +55,6 @@ fn make_change(amt:i32) -> HashMap<String,i32> {
     value_map.insert(String::from("Dollar20"),2000);
     value_map.insert(String::from("Dollar50"),5000);
     value_map.insert(String::from("Dollar100"),10000);
-    // value_map.insert("Penny", 1);
-    // value_map.insert("Nickel",5);
-    // value_map.insert("Dime",10);
-    // value_map.insert("Quarter",25);
-    // value_map.insert("Dollar1",100);
-    // value_map.insert("Dollar5",500);
-    // value_map.insert("Dollar10",1000);
-    // value_map.insert("Dollar20",2000);
-    // value_map.insert("Dollar50",5000);
-    // value_map.insert("Dollar100",10000);
-
 
     let mut change_counter: HashMap<String,i32> = HashMap::new();
     change_counter.insert(String::from("Penny"), 0);
@@ -83,62 +68,23 @@ fn make_change(amt:i32) -> HashMap<String,i32> {
     change_counter.insert(String::from("Dollar50"),0);
     change_counter.insert(String::from("Dollar100"),0);
 
-    let change_amt:i32 = 0;
-    let delta:i32 = amt - change_amt;
+    let mut change_amt:i32 = 0;
+    let mut delta:i32 = amt - change_amt;
     while delta > 0 {
         let filtered_map = filter_map(value_map.clone(),delta);
         let max_val = select_max(filtered_map,delta);
 
-        // let key = max_val.keys().next(); // get the key
-        // let val = max_val.values().next(); // get the value
-
-        for (k,v) in max_val.iter() {
-            let change_val = change_counter[k];
-            // change_counter.insert(*k, change_val+1);
-            change_counter.entry(k).or_insert(change_val+1);
-        }
+        let ky = max_val.0; // get the key
+        let v = max_val.1; // get the value
+        let change_val = change_counter[&ky]; // get the value from the change counter
+        change_counter.insert(ky,change_val+1); // update the value
+        change_amt += v; 
+        delta = amt - change_amt;
     }
     return change_counter;
 }
 
 fn main() {
-    let mut value_map: HashMap<String,i32> = HashMap::new();
-    value_map.insert(String::from("Penny"),1);
-    value_map.insert(String::from("Nickel"),5);
-    value_map.insert(String::from("Dime"),10);
-    value_map.insert(String::from("Quarter"),25);
-    value_map.insert(String::from("Dollar1"),100);
-    value_map.insert(String::from("Dollar5"),500);
-    value_map.insert(String::from("Dollar10"),1000);
-    value_map.insert(String::from("Dollar20"),2000);
-    value_map.insert(String::from("Dollar50"),5000);
-    value_map.insert(String::from("Dollar100"),10000);
-    // value_map.insert("Penny", 1);
-    // value_map.insert("Nickel",5);
-    // value_map.insert("Dime",10);
-    // value_map.insert("Quarter",25);
-    // value_map.insert("Dollar1",100);
-    // value_map.insert("Dollar5",500);
-    // value_map.insert("Dollar10",1000);
-    // value_map.insert("Dollar20",2000);
-    // value_map.insert("Dollar50",5000);
-    // value_map.insert("Dollar100",10000);
-
-
-    let mut change_counter: HashMap<String,i32> = HashMap::new();
-    change_counter.insert(String::from("Penny"), 0);
-    change_counter.insert(String::from("Nickel"),0);
-    change_counter.insert(String::from("Dime"),0);
-    change_counter.insert(String::from("Quarter"),0);
-    change_counter.insert(String::from("Dollar1"),0);
-    change_counter.insert(String::from("Dollar5"),0);
-    change_counter.insert(String::from("Dollar10"),0);
-    change_counter.insert(String::from("Dollar20"),0);
-    change_counter.insert(String::from("Dollar50"),0);
-    change_counter.insert(String::from("Dollar100"),0);
-
-
-    // print_map(value_map);
     let args: std::vec::Vec<_> = env::args().collect();
     if args.len() > 2 {
         println!("Error: Please only enter one value, not multiple");
@@ -154,14 +100,6 @@ fn main() {
     let new_amt:f32 = amt.parse().unwrap(); // convert amount to a float
     let amt_int:i32 = (new_amt * 100.00) as i32;  // convert to int and to cents
 
-    let temp_max = select_max(value_map,amt_int);
-    println!("The original is {}",new_amt);
-    println!("The amount is {}",amt_int);
-    print_map(temp_max);
-
-    // let test = filter_map(value_map,26);
-    // for (k,v) in test.iter() {
-    //     println!("{} goes with {}",k,v)
-    // }
-
+    let result = make_change(amt_int);
+    print_map(result);
 }
