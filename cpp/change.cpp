eb526ae9920a198c2837fb488f367d8b01370a92
wrp801 <wes.poulsen@gmail.com>
2022-06-12 20:48:32 -0600

    Updated c++ implementaiton

--- cpp/change.cpp
+++ cpp/change.cpp
@@ -8,11 +8,14 @@ int main(int argc, char const *argv[])
 		cout << "Error: Please input an amount as an argument" << endl;
 		exit(EXIT_FAILURE);
 	}
-	ChangeMap cm = ChangeMap(false);
-	ChangeMap counter = ChangeMap(true);
-	// float amount_to_change = 12.32;
-	float amount_to_change =  std::stof(argv[1]); // convert the command line arg to a float
-	make_change(cm,&counter,amount_to_change);
-	counter.printMap();
+	for (int i = 1; i < argc;i++)
+	{
+		ChangeMap cm = ChangeMap(false);
+		ChangeMap counter = ChangeMap(true);
+		double amount_to_change =  std::stod(argv[i]); // convert the command line arg to a float
+		cout << "For " << "$" << amount_to_change << ":" << endl;
+		make_change(cm,&counter,amount_to_change);
+		counter.printMap();
+	}
 	return 0;
 }
