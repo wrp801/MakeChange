c09e880237ba4aab54eafa64321fbd9a6197d410
wrp801 <wes.poulsen@gmail.com>
2022-06-12 20:25:28 -0600

    Updated python and julia

--- julia/change.jl
+++ julia/change.jl
@@ -21,7 +21,7 @@ function print_dict(coins::Base.Array{String,1},d::Base.Dict{String,Int64})
 	for c in coins
 		v = get(d,c,0) ## retrieve the value from the map
 		if v != 0
-			println("$c: $v")
+			println("\t$c: $v")
 		end
 	end
 end
@@ -58,6 +58,7 @@ end
 function main(amt::String)
 	coins = ["Penny","Nickel","Dime","Quarter","Dollar1","Dollar5","Dollar10","Dollar20","Dollar50","Dollar100"]
 	amt_val = parse(Float32,amt)
+	println("For \$$amt:")
 	cent_amt = Integer(round(amt_val * 100,digits = 0))
 	filtered_dict = make_change(cent_amt)
 	coins_filt = filter(c -> c in keys(filtered_dict),coins)
@@ -65,4 +66,6 @@ function main(amt::String)
 end
 
 ## Run the script 
-main(ARGS[1])
+for a in ARGS
+	main(a)
+end
