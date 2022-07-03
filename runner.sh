a3878f23811c8147d7ffc3e23b8c360a3f1b7323
wrp801 <wes.poulsen@gmail.com>
2022-07-03 16:21:35 -0600

    Added julia environment

--- runner.sh
+++ runner.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+rm -fr "comparison.txt"
+touch "comparison.txt" ## create the output results
 ## run python
-python3 python/change.py -c
+python3 python/change.py -c >> "comparison.txt"
 
 sleep 2
 
-## run julia
-julia julia/change.jl -c
+# ## run julia
+julia julia/change.jl -c >> "comparison.txt"
 
-sleep 2
-## run c 
-cd ./c 
-make clean 
-make all
-cd ../ ## go back to the MakeChange directory
-./c/change -c
+# sleep 2
+# ## run c 
+# cd ./c 
+# make clean 
+# make all
+# cd ../ ## go back to the MakeChange directory
+# ./c/change -c
 
-sleep 1
+# sleep 1
 
 
